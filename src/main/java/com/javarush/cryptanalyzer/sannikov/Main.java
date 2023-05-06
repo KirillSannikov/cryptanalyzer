7ba4bd18c8c4f2c07c5063039351cf38ece9e628
Kirill Sannikov <kirillsannikov10@gmail.com>
2023-05-06 19:51:10 +1200

    Initialisation

--- src/main/java/com/javarush/cryptanalyzer/sannikov/Main.java
+++ src/main/java/com/javarush/cryptanalyzer/sannikov/Main.java
@@ -1,12 +1,12 @@
 package com.javarush.cryptanalyzer.sannikov;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import com.javarush.cryptanalyzer.sannikov.exception.FileException;
+
+import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.Date;
+import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Main {
     public static int modulo(int n, int modulus) {
@@ -14,39 +14,210 @@ public class Main {
         return n%modulus;
     }
     public static void main(String[] args) {
+         String text = "";
+        try (BufferedReader bufferedWriter = new BufferedReader (new FileReader("C:\\Users\\user\\IdeaProjects\\cryptanalyzer\\src\\main\\java\\com\\javarush\\cryptanalyzer\\sannikov\\file\\input.txt"))) {
+            while (bufferedWriter.ready()){
+                 text = bufferedWriter.readLine();
 
+
+
+
+            }
+        } catch (FileException | IOException e){
+            System.out.println("пустой файл" + e.getMessage());
+        }
+        System.out.println(text);
 
         Scanner console = new Scanner(System.in);
         System.out.println("Для шифрования введите 1, расшифрования – 2, метод brute force (поиск грубой силой) - 3 ");
-//        int number = console.nextInt();
-//
-//
-//        switch (number) {
-//
-//            case 1:
+        int number = console.nextInt();
+
+
+        switch (number) {
+
+            case 1:
                 System.out.println("Введите ключ ");
                 int encryption_key = console.nextInt();
-                System.out.println("Введите текст для шифрования ");
-                String line = console.next();
+//                System.out.println("Введите текст для шифрования ");
+//                String line = console.next();
                 console.close();
 
-                char[] symbol = new char[line.length()];
+                char[] symbol = new char[text.length()];
 
-                for (int i = 0; i < line.length(); i++) {
-                    if (Character.isLetter(line.charAt(i)) && Character.isUpperCase(line.charAt(i))){
-                    symbol[i] = (char) ((modulo((((int) (line.charAt(i))) - 'А' + encryption_key), 33)) + 'А');
+                for (int i = 0; i < text.length(); i++) {
+                    if (Character.isLetter(text.charAt(i)) && Character.isUpperCase(text.charAt(i))){
+                    symbol[i] = (char) ((modulo((((int) (text.charAt(i))) - 'А' + encryption_key), 33)) + 'А');
                     }
 
-                    else if (Character.isLetter(line.charAt(i)) && Character.isLowerCase(line.charAt(i))) {
-                        symbol[i] = (char) ((modulo((((int) (line.charAt(i))) - 'а' + encryption_key), 33)) + 'а');
+                    else if (Character.isLetter(text.charAt(i)) && Character.isLowerCase(text.charAt(i))) {
+                        symbol[i] = (char) ((modulo((((int) (text.charAt(i))) - 'а' + encryption_key), 33)) + 'а');
                     }
                     else {
-                        symbol[i] = line.charAt(i);
+                        symbol[i] = text.charAt(i);
                     }
                 }
                 String line2 = String.copyValueOf(symbol);
 
                 System.out.println("Зашифрованный текст: " + line2);
+                break;
+
+            case 2:
+                System.out.println("Введите ключ ");
+                int encryption_key1 = console.nextInt();
+                System.out.println("Введите текст для шифрования ");
+                String line1 = console.next();
+                console.close();
+
+                char[] symbol1 = new char[line1.length()];
+
+                for (int i = 0; i < line1.length(); i++) {
+                    if (Character.isLetter(line1.charAt(i)) && Character.isUpperCase(line1.charAt(i))) {
+                        symbol1[i] = (char) (((((int) (line1.charAt(i))) - 'А' - encryption_key1) % 33) + 'А');
+                    }
+                    else if (Character.isLetter(line1.charAt(i)) && Character.isLowerCase(line1.charAt(i))) {
+                        symbol1[i] = (char) ((modulo((((int) (line1.charAt(i))) - 'а' - encryption_key1), 33)) + 'а');
+                    }
+                    else {
+                        symbol1[i] = line1.charAt(i);
+                    }
+                }
+                String line21 = String.copyValueOf(symbol1);
+
+                System.out.println("Зашифрованный текст: " + line21);
+                break;
+
+            case 3:
+                System.out.print ("Введите зашифрованный текст, для взлома методом brute force ");
+                String line3 = console.next();
+                console.close();
+                char[] symbol3 = new char[line3.length()];
+
+                int encryption_key3 = 1;
+                while (encryption_key3!=34){
+
+                    for (int i = 0; i < line3.length(); i++) {
+                        if (Character.isLetter(line3.charAt(i)) && Character.isUpperCase(line3.charAt(i))){
+                            symbol3[i] = (char) (((((int) (line3.charAt(i))) - 'А' - encryption_key3 + 33) % 33) + 'А');
+                        }
+                        if (Character.isLetter(line3.charAt(i)) && Character.isLowerCase(line3.charAt(i))){
+                            symbol3[i] = (char) (((((int) (line3.charAt(i))) - 'а' - encryption_key3 + 33) % 33) + 'а');
+                        }
+                        else {
+                            symbol3[i] = line3.charAt(i);
+                        }
+
+                    }
+
+                    System.out.print ("Расшифровка текста " + line3 + " c ключом " + encryption_key3 + ": " );
+                    System.out.println (symbol3);
+                    encryption_key3 ++;
+
+                }
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//    public static int modulo(int n, int modulus) {
+//        if(n < 0) return n%modulus + modulus;
+//        return n%modulus;
+//    }
+//    public static void main(String[] args) {
+//        String text = "";
+//        try (BufferedReader bufferedWriter = new BufferedReader (new FileReader("C:\\Users\\user\\IdeaProjects\\cryptanalyzer\\src\\main\\java\\com\\javarush\\cryptanalyzer\\sannikov\\file\\input.txt"))) {
+//            while (bufferedWriter.ready()){
+//                text = bufferedWriter.readLine();
+//            }
+//        } catch (FileException | IOException e){
+//            System.out.println("пустой файл" + e.getMessage());
+//        }
+//        System.out.println(text);
+//
+//        Scanner console = new Scanner(System.in);
+//        System.out.println("Для шифрования введите 1, расшифрования – 2, метод brute force (поиск грубой силой) - 3 ");
+////        int number = console.nextInt();
+////
+////
+////        switch (number) {
+////
+////            case 1:
+//        System.out.println("Введите ключ ");
+//        int encryption_key = console.nextInt();
+////                System.out.println("Введите текст для шифрования ");
+////                String line = console.next();
+//        console.close();
+//
+//        char[] symbol = new char[text.length()];
+//
+//        for (int i = 0; i < text.length(); i++) {
+//            if (Character.isLetter(text.charAt(i)) && Character.isUpperCase(text.charAt(i))){
+//                symbol[i] = (char) ((modulo((((int) (text.charAt(i))) - 'А' + encryption_key), 33)) + 'А');
+//            }
+//
+//            else if (Character.isLetter(text.charAt(i)) && Character.isLowerCase(text.charAt(i))) {
+//                symbol[i] = (char) ((modulo((((int) (text.charAt(i))) - 'а' + encryption_key), 33)) + 'а');
+//            }
+//            else {
+//                symbol[i] = text.charAt(i);
+//            }
+//        }
+//        String line2 = String.copyValueOf(symbol);
+//
+//        System.out.println("Зашифрованный текст: " + String.format(line2));
 //                break;
 //
 //            case 2:
@@ -105,38 +276,7 @@ public class Main {
 //
 //        }
 
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//    }
 
 
 
