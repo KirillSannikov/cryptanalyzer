83d5def13b5bffe43de71ccd88eb9a72df853c0d
KirillSannikov <132100170+KirillSannikov@users.noreply.github.com>
2023-05-15 17:07:58 +1200

    Delete cryptanalyzer directory

--- cryptanalyzer/src/main/java/com/javarush/cryptanalyzer/sannikov/constants/constForEncryption/ConstDefaultText.java
+++
@@ -1,8 +0,0 @@
-package com.javarush.cryptanalyzer.sannikov.constants.constForEncryption;
-
-public class ConstDefaultText {
-    public final String defaultText = "Введите текст для расшифрования ";
-    public final String enterKey = "Введите ключ ";
-    public final String exceptionFile = "пустой файл";
-
-}
